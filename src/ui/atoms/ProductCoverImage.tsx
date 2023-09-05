c0e9989253c002898773bc5dfa4d0b01a1a555ac
Damian Kalek <dkalek@gmail.com>
2023-09-05 19:28:31 +0200

    Working on lecture 1

---
+++ src/ui/atoms/ProductCoverImage.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export const ProductCoverImage = ({ src, alt }: { src: string; alt: string }) => {
+	return (
+		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-100">
+			<Image
+				className="h-full w-full object-cover object-center p-4 transition-transform hover:scale-105"
+				src={src}
+				alt={alt}
+				width={320}
+				height={320}
+			/>
+		</div>
+	);
+};
